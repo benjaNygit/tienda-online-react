47d0a7a17daf9d556cb6ba429c00c80ed5a5f98f
benjaNygit <benja.ignacio2001@gmail.com>
2022-08-14 16:22:11 -0400

    Agregar componentes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>
+//);
+
+// formas de crear elementos
+const x = React.createElement('p', null, 'Hello World! - x')
+const y = <p>Hello World! - y</p>
+const Z = () => <p>Hello World! - z</p> //jsx
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // x
+  // y
+  <Z/>
 );
 
 // If you want to start measuring performance in your app, pass a function
