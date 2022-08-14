7f8a55dae50a24ef6b223c29fee6e0522099ab21
benjaNygit <benja.ignacio2001@gmail.com>
2022-08-14 19:21:21 -0400

    Estilos con archivos css

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-// inline style
-const estilo = {
-  backgroundColor: '#456',
-  color: '#fff',
-  padding: '10px 15px',
-  margin: '10px 15px',
-}
-
-// inline style con funcion
-const estilo1 = (bg='#456') => ({
-  backgroundColor: bg,
-  color: '#fff',
-  padding: '10px 15px',
-  margin: '10px 15px',
-})
-
-// convinando estilos
-const compuesto = {
-  boxShadow: '0 5px 3px rgba(0,0,0,0.5)',
-}
-
+// usamos estilos importados desde App.css
 const App = () => {
   return (
-    //<p style={estilo}>Hello World!</p>
-    //<p style={estilo1('#333')}>Hello World!</p>
-    <p style={{...estilo1('#333'), ...compuesto}}>Hello World!</p>
+    <p className='estilo sombra'>Hello World!</p>
   );
 }
 
