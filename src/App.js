d2768c42764c1aa9e2b39f74f245845dc324104b
benjaNygit <benja.ignacio2001@gmail.com>
2022-08-15 13:55:29 -0400

    Componentes basados en clases

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import Button from './Button.js'
+import { Component } from 'react'
 
-const arr = [
-  'Hello World! - 1',
-  'Hello World! - 2',
-  'Hello World! - 3',
-]
-
-const App = () => {
-  // si no ponemos key, nos arrojara error la consola del navegador
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      {arr.map(el => <p key={el}>{el}</p>)}
-      <Button onClick={() => console.log('Hello World!')}>Enviar</Button>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <p>Hello World!</p>
+    );
+  }
 }
 
 export default App;
