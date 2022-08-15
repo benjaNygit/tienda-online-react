40113bb1fe2290e4de3da63c2324b2fa99fdb9ec
benjaNygit <benja.ignacio2001@gmail.com>
2022-08-15 14:15:40 -0400

    El state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { Component } from 'react'
 
 class App extends Component {
+  // los state los podemos pasar como propiedades a los componentes
+  state = {
+    valor: 3,
+    className: 'clase',
+  }
+
+  // el render se ejecuta en dos condiciones, si cambia es estado del componente
+  // o si un componente padre a sido llamado
   render() {
+    console.log(this.state);
     return (
-      <p>Hello World!</p>
+      <div>
+        <p>Hello World!</p>
+        <button
+          className={`${this.state.className}`}
+          onClick={() => this.setState({valor: 2})}>
+          Enviar
+        </button>
+      </div>
     );
   }
 }
