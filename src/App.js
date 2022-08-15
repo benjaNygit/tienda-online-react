f00888e1b89e8f445e434f07c201e2baf51ac00e
benjaNygit <benja.ignacio2001@gmail.com>
2022-08-15 01:29:39 -0400

    Listas y keys en React

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Button from './Button.js'
 
+const arr = [
+  'Hello World! - 1',
+  'Hello World! - 2',
+  'Hello World! - 3',
+]
+
 const App = () => {
-  const condicion = true;
-
-  if (condicion) {
-    return (
-      <p>La condición es verdadera!</p>
-    )
-  }
-
+  // si no ponemos key, nos arrojara error la consola del navegador
   return (
     <div>
       <h1>Hello World!</h1>
+      {arr.map(el => <p key={el}>{el}</p>)}
       <Button onClick={() => console.log('Hello World!')}>Enviar</Button>
     </div>
   );
