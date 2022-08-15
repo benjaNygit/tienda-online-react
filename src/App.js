b921054301e13b6880eb255cb436e252be9ac692
benjaNygit <benja.ignacio2001@gmail.com>
2022-08-15 01:12:49 -0400

    Renderizar segun condiciones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import Button from './Button.js'
 
 const App = () => {
+  const condicion = true;
+
+  if (condicion) {
+    return (
+      <p>La condición es verdadera!</p>
+    )
+  }
+
   return (
     <div>
       <h1>Hello World!</h1>
